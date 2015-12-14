4061c1f7176b85b6a4291339b3513a0535ea9ae4
Collin Berg <berg.collin@gmail.com>
2015-12-14 13:59:43 -0600

    added main.js

---
+++ _assets/js/main.js
@@ -0,0 +1,10 @@
+/*============
+* 
+* Main Javascript for Tulsa Ipsum
+*
+* =================*/
+
+// Create a Sample Paragraph of Tulsa Ipsum
+// Allow User to Select if they want a Header, Sentance, or Paragraph 
+// Allow User to Select Number, Max of 5. Possibly on a slider unit
+// Submit will generate the Text in an area, and another button will copy that area to clipboard
