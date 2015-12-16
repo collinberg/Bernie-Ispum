0b98d7a339d02121f0ab95d71aa98c359e673d92
Collin Berg <berg.collin@gmail.com>
2015-12-16 15:03:32 -0600

    styled the label and it updates as you drag now

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -9,6 +9,15 @@
 // Allow User to Select Number, Max of 5. Possibly on a slider unit
 // Submit will generate the Text in an area, and another button will copy that area to clipboard
 var slideNumber = $('#rangeLabel');
-var rangeSlide = $('#Number').val();
+var rangeSlide = $('#Number');
 
-slideNumber.text(rangeSlide);
+
+//Set the Text of the label on Load
+slideNumber.text(rangeSlide.val() );
+
+function labelUpdate(){
+	var updated = rangeSlide.val();
+	slideNumber.text(updated);
+}
+
+$("input[type=range]").mousemove(labelUpdate);
