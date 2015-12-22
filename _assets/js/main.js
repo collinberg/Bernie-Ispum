f4b01767f510da673194133bf1b96a20384cf7d8
Collin Berg <berg.collin@gmail.com>
2015-12-21 23:09:20 -0600

    added radio buttons and more styles to center design

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,21 +3,27 @@
 * Main Javascript for Tulsa Ipsum
 *
 * =================*/
-
-// Create a Sample Paragraph of Tulsa Ipsum
-// Allow User to Select if they want a Header, Sentance, or Paragraph 
-// Allow User to Select Number, Max of 5. Possibly on a slider unit
-// Submit will generate the Text in an area, and another button will copy that area to clipboard
 var slideNumber = $('#rangeLabel');
 var rangeSlide = $('#Number');
-
 
 //Set the Text of the label on Load
 slideNumber.text(rangeSlide.val() );
 
+// Create a Sample Paragraph of Tulsa Ipsum
+// Allow User to Select if they want a Header, Sentance, or Paragraph 
+
+
+
+
+
+
+
+// Allow User to Select Number, Max of 5. Possibly on a slider unit
 function labelUpdate(){
 	var updated = rangeSlide.val();
 	slideNumber.text(updated);
 }
 
 $("input[type=range]").mousemove(labelUpdate);
+
+// Submit will generate the Text in an area, and another button will copy that area to clipboard
